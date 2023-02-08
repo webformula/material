1bf4deab2af8e6a754724d8fb8abd9f810b9a808
Ben Rubin <ben@benjaminrubin.com>
2023-02-07 18:24:50 -0600

    change side-sheet layout

--- src/components/side-sheet/component.js
+++ src/components/side-sheet/component.js
@@ -3,26 +3,31 @@ import './component.css';
 import device from '../../core/device.js';
 import util from '../../core/util.js';
 
-
+// TODO confirm we do not want to allow a global main side sheet (.mdw-connect-page-content)
 
 customElements.define('mdw-side-sheet', class MDWSideSheetElement extends HTMLElementExtended {
   #open;
   #backdrop;
   #closeButton;
   #clickBackdropClose = false;
+  #placeHolder;
   #backdropClick_bound = this.#backdropClick.bind(this);
   #closeClick_bound = this.close.bind(this);
 
   constructor() {
     super();
 
-
     this.classList.add('mdw-no-animation');
-    document.body.classList.add('mdw-side-sheet-no-animation');
-    if (this.classList.contains('mdw-connect-page-content')) document.body.classList.add('mdw-side-sheet-connect-page-content');
+    // document.body.classList.add('mdw-side-sheet-no-animation');
+    // if (this.classList.contains('mdw-connect-page-content')) document.body.classList.add('mdw-side-sheet-connect-page-content');
     this.classList.add('mdw-hide');
     this.#open = false;
-    this.#handleState();
+
+    this.#placeHolder = document.createElement('div');
+    this.#placeHolder.classList.add('mdw-side-sheet-placeholder');
+    this.insertAdjacentElement('afterend', this.#placeHolder);
+
+    // this.#handleState();
   }
 
   connectedCallback() {
@@ -30,7 +35,7 @@ customElements.define('mdw-side-sheet', class MDWSideSheetElement extends HTMLEl
     if (this.#closeButton) this.#closeButton.addEventListener('click', this.#closeClick_bound);
     util.nextAnimationFrameAsync().then(() => {
       this.classList.remove('mdw-no-animation');
-      document.body.classList.remove('mdw-side-sheet-no-animation');
+      // document.body.classList.remove('mdw-side-sheet-no-animation');
     });
   }
 
@@ -48,7 +53,7 @@ customElements.define('mdw-side-sheet', class MDWSideSheetElement extends HTMLEl
 
     this.#open = !!value;
     this.classList.toggle('mdw-hide', !this.#open);
-
+    console.log(this.#isModal)
     if (this.#isModal) {
       if (this.#open) {
         if (!this.#backdrop) this.#backdrop = document.createElement('mdw-backdrop');
@@ -60,7 +65,7 @@ customElements.define('mdw-side-sheet', class MDWSideSheetElement extends HTMLEl
       }
     }
 
-    this.#handleState();
+    // this.#handleState();
   }
 
   get clickBackdropClose() {
@@ -91,13 +96,13 @@ customElements.define('mdw-side-sheet', class MDWSideSheetElement extends HTMLEl
     this.open = false;
   }
 
-  #handleState() {
-    document.body.classList.remove('mdw-side-sheet-state-hide');
-    document.body.classList.remove('mdw-side-sheet-state-modal');
-    document.body.classList.remove('mdw-side-sheet-state-open');
+  // #handleState() {
+  //   document.body.classList.remove('mdw-side-sheet-state-hide');
+  //   document.body.classList.remove('mdw-side-sheet-state-modal');
+  //   document.body.classList.remove('mdw-side-sheet-state-open');
 
-    if (this.classList.contains('mdw-hide')) document.body.classList.add('mdw-side-sheet-state-hide');
-    else if (this.#isModal) document.body.classList.add('mdw-side-sheet-state-modal');
-    else document.body.classList.add('mdw-side-sheet-state-open');
-  }
+  //   if (this.classList.contains('mdw-hide')) document.body.classList.add('mdw-side-sheet-state-hide');
+  //   else if (this.#isModal) document.body.classList.add('mdw-side-sheet-state-modal');
+  //   else document.body.classList.add('mdw-side-sheet-state-open');
+  // }
 });
