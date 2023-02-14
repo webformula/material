dc2bab8ac499ab018eb1725d1bbd9a45b16b4e72
Ben Rubin <ben@benjaminrubin.com>
2023-02-13 19:19:44 -0600

    Update forms page Add side sheet page update side sheet

--- src/components/side-sheet/component.js
+++ src/components/side-sheet/component.js
@@ -1,14 +1,13 @@
 import HTMLElementExtended from '../HTMLElementExtended.js';
 import './component.css';
-import device from '../../core/device.js';
 import util from '../../core/util.js';
 
-// TODO confirm we do not want to allow a global main side sheet (.mdw-connect-page-content)
 
 customElements.define('mdw-side-sheet', class MDWSideSheetElement extends HTMLElementExtended {
   #open;
   #backdrop;
-  #closeButton;
+  #backdropElement;
+  #modal;
   #clickBackdropClose = false;
   #placeHolder;
   #backdropClick_bound = this.#backdropClick.bind(this);
@@ -18,30 +17,28 @@ customElements.define('mdw-side-sheet', class MDWSideSheetElement extends HTMLEl
     super();
 
     this.classList.add('mdw-no-animation');
-    // document.body.classList.add('mdw-side-sheet-no-animation');
-    // if (this.classList.contains('mdw-connect-page-content')) document.body.classList.add('mdw-side-sheet-connect-page-content');
     this.classList.add('mdw-hide');
     this.#open = false;
+    if (this.classList.contains('mdw-global')) this.classList.add('mdw-modal')
+    this.#modal = this.classList.contains('mdw-modal');
+    this.#backdrop = this.classList.contains('mdw-backdrop');
+    this.#clickBackdropClose = this.classList.contains('mdw-click-backdrop-close');
 
     this.#placeHolder = document.createElement('div');
     this.#placeHolder.classList.add('mdw-side-sheet-placeholder');
     this.insertAdjacentElement('afterend', this.#placeHolder);
-
-    // this.#handleState();
   }
 
   connectedCallback() {
-    this.#closeButton = this.querySelector('.mdw-side-sheet-close');
-    if (this.#closeButton) this.#closeButton.addEventListener('click', this.#closeClick_bound);
     util.nextAnimationFrameAsync().then(() => {
+      this.querySelectorAll('.mdw-side-sheet-close').forEach(element => element.addEventListener('click', this.#closeClick_bound));
       this.classList.remove('mdw-no-animation');
-      // document.body.classList.remove('mdw-side-sheet-no-animation');
     });
   }
 
   disconnectedCallback() {
-    if (!this.#backdrop) this.#backdrop.remove();
-    if (this.#closeButton) this.#closeButton.removeEventListener('click', this.#closeClick_bound);
+    if (!this.#backdropElement) this.#backdropElement.remove();
+    this.querySelectorAll('.mdw-side-sheet-close').forEach(element => element.removeEventListener('click', this.#closeClick_bound));
   }
 
   get open() {
@@ -54,18 +51,16 @@ customElements.define('mdw-side-sheet', class MDWSideSheetElement extends HTMLEl
     this.#open = !!value;
     this.classList.toggle('mdw-hide', !this.#open);
 
-    if (this.#isModal) {
+    if (this.#modal && this.#backdrop) {
       if (this.#open) {
-        if (!this.#backdrop) this.#backdrop = document.createElement('mdw-backdrop');
-        this.insertAdjacentElement('beforebegin', this.#backdrop);
-        if (this.#clickBackdropClose) this.#backdrop.addEventListener('click', this.#backdropClick_bound);
-      } else if (this.#backdrop) {
-        this.#backdrop.removeEventListener('click', this.#backdropClick_bound);
-        this.#backdrop.remove();
+        if (!this.#backdropElement) this.#backdropElement = document.createElement('mdw-backdrop');
+        this.insertAdjacentElement('beforebegin', this.#backdropElement);
+        if (this.#clickBackdropClose) this.#backdropElement.addEventListener('click', this.#backdropClick_bound);
+      } else if (this.#backdropElement) {
+        this.#backdropElement.removeEventListener('click', this.#backdropClick_bound);
+        this.#backdropElement.remove();
       }
     }
-
-    // this.#handleState();
   }
 
   get clickBackdropClose() {
@@ -76,8 +71,20 @@ customElements.define('mdw-side-sheet', class MDWSideSheetElement extends HTMLEl
     this.#clickBackdropClose = !!value;
   }
 
-  get #isModal() {
-    return this.classList.contains('mdw-modal') || device.isMobile;
+  get modal() {
+    return this.#modal;
+  }
+  set modal(value) {
+    this.#modal = this.classList.contains('mdw-global') ? true : !!value;
+    this.classList.toggle('mdw-modal', this.#modal);
+  }
+
+  get backdrop() {
+    return this.#backdrop;
+  }
+  set backdrop(value) {
+    this.#backdrop = !!value;
+    this.classList.toggle('mdw-backdrop', this.#backdrop);
   }
 
   show() {
@@ -95,14 +102,4 @@ customElements.define('mdw-side-sheet', class MDWSideSheetElement extends HTMLEl
   #backdropClick() {
     this.open = false;
   }
-
-  // #handleState() {
-  //   document.body.classList.remove('mdw-side-sheet-state-hide');
-  //   document.body.classList.remove('mdw-side-sheet-state-modal');
-  //   document.body.classList.remove('mdw-side-sheet-state-open');
-
-  //   if (this.classList.contains('mdw-hide')) document.body.classList.add('mdw-side-sheet-state-hide');
-  //   else if (this.#isModal) document.body.classList.add('mdw-side-sheet-state-modal');
-  //   else document.body.classList.add('mdw-side-sheet-state-open');
-  // }
 });
