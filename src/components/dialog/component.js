6291261260709356b3e291a327edb95a9cce2f93
Ben Rubin <ben@benjaminrubin.com>
2023-03-27 14:25:27 -0500

    add dialog tabbing to internal elements

--- src/components/dialog/component.js
+++ src/components/dialog/component.js
@@ -3,6 +3,9 @@ import './component.css';
 
 export default class MDWDialogElement extends MDWPanelElement {
   #returnValue;
+  #nextFocusable;
+  #lastFocused;
+  #onTab_bound = this.#onTab.bind(this);
 
   constructor() {
     super();
@@ -17,11 +20,32 @@ export default class MDWDialogElement extends MDWPanelElement {
     return this.#returnValue;
   }
 
+  show() {
+    super.show();
+
+    const focusable = [...this.querySelectorAll('*')].find(e => e.tabindex > -1 || parseInt(e.getAttribute('tabindex') || -1) > -1);
+    if (focusable) {
+      this.#nextFocusable = focusable;
+      this.#lastFocused = document.activeElement;
+      window.addEventListener('keydown', this.#onTab_bound);
+    }
+  }
+
   close(returnValue) {
     if (this.open !== true) return;
 
     this.#returnValue = returnValue;
     super.close();
+    if (this.#lastFocused) this.#lastFocused.focus();
+  }
+
+  #onTab(e) {
+    if (e.code === 'Tab') {
+      this.#nextFocusable.focus();
+      this.#nextFocusable = undefined;
+      e.preventDefault();
+      window.removeEventListener('keydown', this.#onTab_bound);
+    }
   }
 }
 
