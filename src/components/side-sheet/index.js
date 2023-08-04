7f2e4f0c3595a2f2dd142a3f475e451775e4d9d4
Ben Rubin <ben@benjaminrubin.com>
2023-08-04 12:27:19 -0500

    up

--- src/components/side-sheet/index.js
+++ src/components/side-sheet/index.js
@@ -10,12 +10,8 @@ customElements.define('mdw-side-sheet', class MDWSideSheetElement extends HTMLEl
   #modal;
   #clickOutsideClose = false;
   #placeHolder;
-  #content;
-  #actions;
-  #header;
   #scrimClick_bound = this.#scrimClick.bind(this);
   #closeClick_bound = this.close.bind(this);
-  #scrolled_bound = this.#scrolled.bind(this);
   #windowState_bound = this.#windowState.bind(this);
 
   constructor() {
@@ -40,20 +36,11 @@ customElements.define('mdw-side-sheet', class MDWSideSheetElement extends HTMLEl
       this.classList.remove('mdw-no-animation');
     });
     window.addEventListener('mdwwindowstate', this.#windowState_bound);
-
-    // this.#content = this.querySelector('.mdw-content');
-    // this.#actions = this.querySelector('.mdw-actions');
-    // this.#header = this.querySelector('.mdw-header');
-    // if (this.#content) {
-    //   this.querySelector('.mdw-content').addEventListener('scroll', this.#scrolled_bound);
-    //   this.#scrolled();
-    // }
   }
 
   disconnectedCallback() {
     if (this.#scrimElement) this.#scrimElement.remove();
     this.querySelectorAll('.mdw-side-sheet-close').forEach(element => element.removeEventListener('click', this.#closeClick_bound));
-    if (this.#content && this.#actions) this.querySelector('.mdw-content').removeEventListener('scroll', this.#scrolled_bound);
     window.removeEventListener('mdwwindowstate', this.#windowState_bound);
   }
 
@@ -121,10 +108,5 @@ customElements.define('mdw-side-sheet', class MDWSideSheetElement extends HTMLEl
 
   #scrimClick() {
     this.open = false;
-  }
-
-  #scrolled() {
-    if (this.#actions) this.#actions.classList.toggle('mdw-scrolled', (this.#content.scrollHeight - this.#content.scrollTop) > this.#content.offsetHeight);
-    if (this.#header) this.#header.classList.toggle('mdw-scrolled', this.#content.scrollTop > 0);
   }
 });
