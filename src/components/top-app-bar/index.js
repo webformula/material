f9ab645a1270d128d0786b4edd0f82274772560b
Ben Rubin <ben@benjaminrubin.com>
2024-02-17 14:07:21 -0600

    refactor top app bar

--- src/components/top-app-bar/index.js
+++ src/components/top-app-bar/index.js
@@ -1,56 +1,103 @@
 import HTMLComponentElement from '../HTMLComponentElement.js';
+import styles from './component.css' assert { type: 'css' };
 import util from '../../core/util.js';
 
-/* TODO refactor */
 
 class WFCTopAppBarElement extends HTMLComponentElement {
   static tag = 'wfc-top-app-bar';
-  #sticky = this.classList.contains('sticky');
-  #compress = this.classList.contains('compress');
-  #height;
-  #scrollTrack_bound = this.#scrollTrack.bind(this);
+  static useShadowRoot = true;
+  static useTemplate = true;
+  static styleSheets = styles;
 
+
+  #position = 0;
+  #fixed = false;
+  #compress = false;
+  #medium = false;
+  #large = false;
+  #height = 64;
+  #scrollTrack_bound = this.#scrollTrack.bind(this);
 
   constructor() {
     super();
+
+    this.render();
+    document.body.classList.add('has-top-app-bar');
+  }
+
+  template() {
+    return /*html*/`
+    <slot name="leading-icon"></slot>
+    <slot class="default-slot"></slot>
+    <slot name="trailing-icon"></slot>
+    `;
   }
 
   connectedCallback() {
-    const isMedium = this.classList.contains('medium');
-    const isLarge = this.classList.contains('large');
-    document.body.classList.add('has-top-app-bar');
-    if (isMedium) document.body.classList.add('top-app-bar-medium');
-    if (isLarge) document.body.classList.add('top-app-bar-large');
-    if (this.#compress && (isMedium || isLarge)) {
-      this.#height = this.offsetHeight - 64;
-    }
-
-    // prevent layout calculation during script evaluation
-    requestAnimationFrame(() => {
-      util.trackPageScroll(this.#scrollTrack_bound);
-    });
+    util.trackPageScroll(this.#scrollTrack_bound);
   }
 
   disconnectedCallback() {
     util.untrackPageScroll(this.#scrollTrack_bound);
   }
 
-  #scrollTrack({ isScrolled, direction, distance, scrollTop }) {
-    // prevent style changes if not moving
-    const position = -parseInt(this.style.getPropertyValue('--wfc-top-app-bar-scroll-position').replace('px', '') || 0);
+  static get observedAttributesExtended() {
+    return [
+      ['fixed', 'boolean'],
+      ['compress', 'boolean'],
+      ['medium', 'boolean'],
+      ['large', 'boolean']
+    ];
+  }
 
-    // adjust scroll check based on header position for scrolled
-    if (this.#compress) this.classList.toggle('scrolled', scrollTop - Math.max(0, position + distance) > 0);
-    else if (this.#sticky) this.classList.toggle('scrolled', isScrolled);
+  attributeChangedCallbackExtended(name, _oldValue, newValue) {
+    this[name] = newValue;
+  }
 
-    if (direction === 1 && position === 0) return;
-    if (direction === -1 && position === this.#height) return;
+  get fixed() { return this.#fixed; }
+  set fixed(value) {
+    this.#fixed = !!value;
+    this.toggleAttribute('fixed', this.#fixed);
+  }
 
-    // move with scroll
-    let value = position + distance;
-    if (value > this.#height) value = this.#height;
-    if (value < 0) value = 0;
-    this.style.setProperty('--wfc-top-app-bar-scroll-position', `${-value}px`);
+  get compress() { return this.#compress; }
+  set compress(value) {
+    this.#compress = !!value;
+    this.toggleAttribute('compress', this.#compress);
+  }
+
+  get medium() { return this.#medium; }
+  set medium(value) {
+    this.#medium = !!value;
+    this.toggleAttribute('medium', this.#medium);
+    document.body.classList.toggle('top-app-bar-medium', this.#medium);
+    this.#height = this.#medium ? 112 : 64;
+  }
+
+  get large() { return this.#large; }
+  set large(value) {
+    this.#large = !!value;
+    this.toggleAttribute('large', this.#large);
+    document.body.classList.toggle('top-app-bar-large', this.#large);
+    this.#height = this.#large ? 152 : 64;
+  }
+
+  #scrollTrack({ isScrolled, distance, scrollTop }) {
+    if (this.#fixed) {
+      this.classList.toggle('scrolled', isScrolled);
+    } else if (this.#compress) {
+      this.#position += distance;
+      if (this.#position > this.#height - 64) this.#position = this.#height - 64;
+      if (this.#position < 0) this.#position = 0;
+      this.classList.toggle('scrolled', scrollTop > 64);
+      this.style.setProperty('--wfc-top-app-bar-scroll-position', `-${this.#position}px`);
+    } else {
+      this.#position += distance;
+      if (this.#position > this.#height) this.#position = this.#height;
+      if (this.#position < 0) this.#position = 0;
+      this.classList.toggle('scrolled', scrollTop > this.#height && this.#position < this.#height);
+      this.style.top = `-${this.#position}px`;
+    }
   }
 }
 
